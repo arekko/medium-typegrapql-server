d4d389cfbfd0b2afd50829d491d25fe3ed726a51
arekko <you@example.com>
2019-03-14 21:12:56 +0200

    added Error type and error messages

--- src/modules/user/Login.ts
+++ src/modules/user/Login.ts
@@ -2,29 +2,41 @@ import bcrypt from "bcryptjs";
 import { MyContext } from "src/types/MyContext";
 import { Arg, Ctx, Mutation, Resolver } from "type-graphql";
 import { User } from "../../entity/User";
+import { invalidLogin } from "./login/ErrorMessages";
+import { Error } from "./shared/Error";
 
 @Resolver()
 export class LoginResolver {
-  @Mutation(() => User, { nullable: true })
+  @Mutation(() => [Error], { nullable: true })
   async login(
     @Arg("email") email: string,
     @Arg("password") password: string,
     @Ctx() ctx: MyContext
-  ): Promise<User | null> {
+  ): Promise<Error[] | null> {
     const user = await User.findOne({ where: { email } });
 
     if (!user) {
-      return null;
+      return [
+        {
+          path: "email",
+          message: invalidLogin
+        }
+      ];
     }
 
     const valid = await bcrypt.compare(password, user.password);
 
     if (!valid) {
-      return null;
+      return [
+        {
+          path: "password",
+          message: "incorrect password"
+        }
+      ];
     }
 
     ctx.req.session!.userId = user.id;
 
-    return user;
+    return null;
   }
 }

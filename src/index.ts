8b5a43ad5edd0a170362048e6d3facfa527e664e
arekko <you@example.com>
2019-03-14 10:33:58 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { ApolloServer } from 'apollo-server-express'
+import * as Express from 'express'
+import { buildSchema} from 'type-graphql'
+
+
+const main = async = () => {
+
+const schema = await buildSchema({
+  resolvers: [FirstResolver, SampleResolver],
+});
+}
+
+
+main()
